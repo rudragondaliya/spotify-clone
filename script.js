e5229e25b1377fe3ab22506514d0cdfd6d8d2c38
rudragondaliya <gondaliyarudra61@gmail.com>
2024-07-05 18:08:04 +0530

    done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $('#search-page,#podcast-page,#sign-page,#login-page,#language-page').hide()
 
 
 $("a").on("click",function(){
-    $("#home-search,#search-page,#podcast-page,#sign-page,#login-page,#language-page").hide();
+    $("#home-page,#search-page,#podcast-page,#sign-page,#login-page,#language-page").hide();
   $($(this).attr("href")).show();
   console.log($(this).attr("href"));
 })
