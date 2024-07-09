2b9a211371b5be986e228de69fe5fcb92a5dd2ec
rudragondaliya <gondaliyarudra61@gmail.com>
2024-07-09 14:03:01 +0530

    done

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ $("a").on("click",function(){
   $($(this).attr("href")).show();
   console.log($(this).attr("href"));
 })
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
